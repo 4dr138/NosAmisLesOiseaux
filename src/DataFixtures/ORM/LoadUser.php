3309732c3dbcb90b3d2dc55b6ef3eb51ac64fea6
Adrien Gautier <agautier38@gmail.com>
2018-03-27 21:09:40 +0200

    Gestion backend sans interaction avec BDD

---
+++ src/DataFixtures/ORM/LoadUser.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\DataFixtures;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Entity\Users;
+
+class LoadUser implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        // Les noms d'utilisateurs à créer
+        $listNames = array('Alexandre', 'Marine', 'Anna');
+        foreach ($listNames as $name) {
+            // On crée l'utilisateur
+            $user = new Users;
+            // Le nom d'utilisateur et le mot de passe sont identiques
+            $user->setUsername($name);
+            $user->setPassword($name);
+            $user->setName($name);
+            $user->setFirstname($name);
+            $user->setmail($name);
+            // On définit uniquement le role ROLE_USER qui est le role de base
+            $user->setRole('ROLE_ADMIN');
+            // On le persiste
+            $manager->persist($user);
+        }
+        // On déclenche l'enregistrement
+        $manager->flush();
+    }
+}
