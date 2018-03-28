afec818e8e5c834e29d9903eef4c4ba90d9797f8
4dr138 <agautier38@gmail.com>
2018-03-28 17:01:18 +0200

    Insertion map

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adrien.gautier
+ * Date: 28/03/2018
+ * Time: 16:38
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class IndexController extends Controller
+{
+
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction()
+    {
+        return $this->render('index/index.html.twig');
+    }
+
+}
