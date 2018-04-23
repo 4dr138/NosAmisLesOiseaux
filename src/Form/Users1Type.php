489d0946edaaa26685f342513f75afae9f58b143
Adrien Gautier <adrien.gautier@asposan.local>
2018-04-23 10:50:30 +0200

    Gestion profil users

--- src/Form/Users1Type.php
+++ src/Form/Users1Type.php
@@ -4,7 +4,10 @@ namespace App\Form;
 
 use App\Entity\Users;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,13 +16,13 @@ class Users1Type extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            //->add('name')
-            //->add('firstname')
-            //->add('username')
-            ->add('mail')
-            ->add('password')
-            ->add('newsletter')
-            ->add('image', FileType::class, array('label' => 'Image(JPG)'))
+            ->add('name', TextType::class, array('label' => 'Nom : ', 'required' => true, ))
+            ->add('firstname', TextType::class, array('label' => 'Prénom : ', 'required' => true, ))
+            ->add('username', TextType::class, array('label' => 'Pseudonyme : ', 'required' => true))
+            ->add('mail', TextType::class, array('label' => 'E-mail : ', 'required' => true))
+            ->add('password', TextType::class, array('label' => 'Mot de passe : ', 'required' => true))
+            ->add('newsletter', CheckboxType::class, array('label' => 'Voulez-vous vous abonner à notre Newsletter ? ', 'required' => false))
+            ->add('image', FileType::class, array('label' => 'Image(JPG)', 'data_class' => null))
             //->add('isActive')
             //->add('roles')
         ;
