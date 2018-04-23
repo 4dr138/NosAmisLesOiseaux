98b1f6f3e49fc24d885a085b0ef49fd29af49729
Fabien CAGNIET <fabien.cagniet@gmail.com>
2018-04-23 21:47:39 +0200

    add relation between bird and birdStatus

--- src/Entity/BirdStatus.php
+++ src/Entity/BirdStatus.php
@@ -21,6 +21,16 @@ class BirdStatus
      */
     private $label;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Bird", mappedBy="birdStatuses")
+     */
+    private $birds;
+
+    public function __construct()
+    {
+        $this->birds = new ArrayCollection();
+    }
+
     public function getId()
     {
         return $this->id;
@@ -34,6 +44,37 @@ class BirdStatus
     public function setLabel(string $label): self
     {
         $this->label = $label;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Bird[]
+     */
+    public function getBirds(): Collection
+    {
+        return $this->birds;
+    }
+
+    public function addBird(Bird $bird): self
+    {
+        if (!$this->birds->contains($bird)) {
+            $this->birds[] = $bird;
+            $bird->setBirdStatuses($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBird(Bird $bird): self
+    {
+        if ($this->birds->contains($bird)) {
+            $this->birds->removeElement($bird);
+            // set the owning side to null (unless already changed)
+            if ($bird->getBirdStatuses() === $this) {
+                $bird->setBirdStatuses(null);
+            }
+        }
 
         return $this;
     }
