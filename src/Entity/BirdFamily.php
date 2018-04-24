d98dd49e0cc986a7e63fb74c316e61a894dd60fc
Fabien CAGNIET <fabien.cagniet@gmail.com>
2018-04-24 09:41:21 +0200

    add Bird, BirdStatus and BirdFamily and its repository

---
+++ src/Entity/BirdFamily.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\BirdFamilyRepository")
+ */
+class BirdFamily
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $label;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Bird", mappedBy="birdFamilies")
+     */
+    private $birds;
+
+    public function __construct()
+    {
+        $this->birds = new ArrayCollection();
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): self
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Bird[]
+     */
+    public function getBirds(): Collection
+    {
+        return $this->birds;
+    }
+
+    public function addBird(Bird $bird): self
+    {
+        if (!$this->birds->contains($bird)) {
+            $this->birds[] = $bird;
+            $bird->setBirdFamilies($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBird(Bird $bird): self
+    {
+        if ($this->birds->contains($bird)) {
+            $this->birds->removeElement($bird);
+            // set the owning side to null (unless already changed)
+            if ($bird->getBirdFamilies() === $this) {
+                $bird->setBirdFamilies(null);
+            }
+        }
+
+        return $this;
+    }
+}
