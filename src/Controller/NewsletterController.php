eee231aace88caf1a40571e4df1725c24487f128
todaik <wbarla33@gmail.com>
2018-05-07 16:45:36 +0200

    newsletter
    abonnement newsletter in database

---
+++ src/Controller/NewsletterController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Newsletter;
+use App\Form\NewsletterType;
+use App\Repository\NewsletterRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/newsletter")
+ */
+class NewsletterController extends Controller
+{
+
+    /**
+     * @Route("/new", name="newsletter_new", methods="GET|POST")
+     */
+    /*public function new(Request $request): Response
+    {
+        $newsletter = new Newsletter();
+        $form = $this->createForm(NewsletterType::class, $newsletter);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($newsletter);
+            $em->flush();
+
+             $this->addFlash("success", "Merci pour votre inscription à la newsletter !");
+            return $this->redirectToRoute('homepage');
+        }
+
+        return $this->render('newsletter/new.html.twig', [
+            'newsletter' => $newsletter,
+            'form' => $form->createView(),
+        ]);
+    }
+*/
+    
+
+    
+
+    
+}
