58ca7a7b388eab3011b76537e02cafe02d3e21c4
4dr138 <31504718+4dr138@users.noreply.github.com>
2018-05-08 15:52:40 +0200

    Update IndexController.php

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -7,7 +7,6 @@ use App\Form\NewsletterType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 
