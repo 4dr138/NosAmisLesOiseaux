cad6854d7b4de499130c799ecd6d2b0a19730da4
4dr138 <31504718+4dr138@users.noreply.github.com>
2018-05-08 22:08:14 +0200

    fixing obs + error map homepage

--- public/assets/js/maps/mapsHomepage.js
+++ public/assets/js/maps/mapsHomepage.js
@@ -1,5 +1,6 @@
 function initMap(observations = null) {
 
+
     var map = new google.maps.Map(document.getElementById('map'), {
         zoom: 5,
         center: {lat: 46.528635, lng: 2.4389648} // france
@@ -20,10 +21,16 @@ function initMap(observations = null) {
             dataType: "json",
             success: function(data){
                 for(var i = 0; i < data.length; i++) {
+                    lat = data[i].latitude;
+                    latitude = parseFloat(lat);
+
+                    long = data[i].longitude;
+                    longitude = parseFloat(long);
+
                     locations[i] = [];
                     locations[i] = {
-                        'lat': data[i].latitude,
-                        'lng': data[i].longitude
+                        'lat': latitude,
+                        'lng': longitude
                     }
                 }
 
@@ -43,27 +50,42 @@ function initMap(observations = null) {
             }
         });
     } else {
-        for(var i = 0; i < observations.length; i++) {
-            locations[i] = [];
-            locations[i] = {
-                'lat': observations[i].latitude,
-                'lng': observations[i].longitude
-            }
+        if(observations.length == 0)
+        {
+            $("#alert_map").remove();
+            $('<p id="alert_map" style = "color: red;text-align: center;">Aucune observation enregistrée chez cette espèce pour le moment</p>').insertAfter($("#map"));
         }
-        // locations = observations;
+        else {
+            console.log(observations.length);
 
-        console.log('locations quand recherche');
-        console.log(locations);
+            for (var i = 0; i < observations.length; i++) {
+                lat = observations[i].latitude;
+                latitude = parseFloat(lat);
 
-        var markers = locations.map(function(location, i) {
-            return new google.maps.Marker({
-                position: location,
-                label: labels[i % labels.length]
+                long = observations[i].longitude;
+                longitude = parseFloat(long);
+
+                locations[i] = [];
+                locations[i] = {
+                    'lat': latitude,
+                    'lng': longitude
+                }
+            }
+            // locations = observations;
+
+            console.log('locations quand recherche');
+
+
+            var markers = locations.map(function (location, i) {
+                return new google.maps.Marker({
+                    position: location,
+                    label: labels[i % labels.length],
+                });
             });
-        });
 
-        // Add a marker clusterer to manage the markers.
-        var markerCluster = new MarkerClusterer(map, markers,
-            {imagePath: "../../../assets/js/maps/images/m"});
+            // Add a marker clusterer to manage the markers.
+            var markerCluster = new MarkerClusterer(map, markers,
+                {imagePath: "../../../assets/js/maps/images/m"});
+        }
     }
 }
